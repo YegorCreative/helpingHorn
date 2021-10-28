dad69243cfc67949591c39ca7f23ba8a2308bf0a
YegorCreative <37757944+YegorCreative@users.noreply.github.com>
2021-10-27 23:05:43 -0500

    all new projects

--- javaScript/app.js
+++ javaScript/app.js
@@ -34,30 +34,3 @@ function myFunction() {
   }
 
 
-
-
-  function openCity(cityName, elmnt, color) {
-    // Hide all elements with class="tabcontent" by default */
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-  
-    // Remove the background color of all tablinks/buttons
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].style.backgroundColor = "";
-    }
-  
-    // Show the specific tab content
-    document.getElementById(cityName).style.display = "flex";
-    document.getElementById(cityName).style.flexDirection = "column";
-    document.getElementById(cityName).style.boxSizing = "border-Box";
-    document.getElementById(cityName).style.marginTop = "40px";
-    // Add the specific color to the button used to open the tab content
-    elmnt.style.backgroundColor = color;
-  }
-  
-  // Get the element with id="defaultOpen" and click on it
-  document.getElementById("defaultOpen").click();
