66c64016cdf483e75f29d4d6bb9522ea5bb1f881
YegorCreative <37757944+YegorCreative@users.noreply.github.com>
2021-11-02 00:02:46 -0500

    all for each

--- javaScript/app.js
+++ javaScript/app.js
@@ -38,3 +38,20 @@ function myFunction() {
 
 
 // Remove the transition class
+// Remove the transition class
+const square = document.querySelector('.square');
+square.classList.remove('square-transition');
+
+// Create the observer, same as before:
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      square.classList.add('square-transition');
+      return;
+    }
+
+    square.classList.remove('square-transition');
+  });
+});
+
+observer.observe(document.querySelector('.square-wrapper'));
